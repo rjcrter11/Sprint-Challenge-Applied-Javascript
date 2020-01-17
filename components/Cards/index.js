7e4af31209bca267e15817d5f7c5cb554f841750
rjcrter11 <rjcrter11@gmail.com>
2020-01-17 11:24:51 -0600

    possibly getting somewhere close to figuring out the cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardsContainer = document.querySelector(".cards-container");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then((response) => {
+    console.log(Object.values(response.data.articles.javascript));
+  })
+  .catch((error) => {
+    console.log("data not retrieved", error);
+  });
+
+function lambdaCards(data) {
+  // Select Elements
+
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const name = document.createElement("span");
+
+  // Append Elements
+
+  card.append(headline, author);
+  author.append(imgContainer, name);
+  imgContainer.append(img);
+
+  // Add Classes
+
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  // Add Properties
+
+  headline.textContent = data.headline;
+  img.src = data.authorPhoto;
+  name.textContent = `By ${data.authorName}`;
+
+  return card;
+}
